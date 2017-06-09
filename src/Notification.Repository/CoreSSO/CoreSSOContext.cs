e77e01dfb4389cbf934fb2f21ef320e34f0eb86f
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-06-09 15:44:09 -0300

    [API CoreSSO] Criado a consulta de Sistema

---
+++ src/Notification.Repository/CoreSSO/CoreSSOContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Repository.CoreSSO
+{
+    public abstract class CoreSSORepository
+    {
+        internal readonly string stringConnection;
+
+        public CoreSSORepository()
+        {
+            stringConnection = Connection.Get("CoreSSO");
+        }
+    }
+}
