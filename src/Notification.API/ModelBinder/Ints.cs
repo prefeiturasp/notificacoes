a5440ea8a6a3a301dad38e3041f9cdb3f0a5f471
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-06-14 11:12:11 -0300

    [API SGP]  Adicionado as ModelBinder para receber listas

---
+++ src/Notification.API/ModelBinder/Ints.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+
+namespace Notification.API.ModelBinder
+{
+    public class Ints : IModelBinder
+    {
+        public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
+        {
+            if (bindingContext.ModelType != typeof(IEnumerable<int>))
+            {
+                return false;
+            }
+
+            var intValues = actionContext.Request.GetQueryNameValuePairs()
+            .Where(kvp => kvp.Key == bindingContext.ModelName)
+            .Select(kvp => int.Parse(kvp.Value))
+            .ToList();
+            bindingContext.Model = new List<int>(intValues);
+
+            return true;
+        }
+    }
+}
