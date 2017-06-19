b094e58cf5a40d86ff2ed7d41eabf2488af0a515
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-06-19 17:37:15 -0300

    [Serviço de notificação] Arquitetura basica da aplicação

---
+++ src/Notification.SignalRServer/SignalR/NotificationHub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Notification.Business;
+
+namespace Notification.SignalRServer.SignalR
+{
+    public class NotificationHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            LogBusiness.Info(string.Format("[NotificationHub] OnConnected (connectionId: {0})", Context.ConnectionId));
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            LogBusiness.Info(string.Format("[NotificationHub] OnReconnected (connectionId: {0})", Context.ConnectionId));
+            return base.OnReconnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            LogBusiness.Info(string.Format("[NotificationHub] OnDisconnected (connectionId: {0})", Context.ConnectionId));
+            return base.OnDisconnected(stopCalled);
+        }
+        
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
