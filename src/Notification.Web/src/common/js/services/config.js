f01e3ae11f59b38896ce551499b8e20e06b78d6d
MSTECH\everton.ferreira <everton.ferreira@mstech.com.br>
2017-06-21 15:47:03 -0300

    20000019-58 [Interface de notificação] - melhoria no carregamento da tela

--- src/Notification.Web/src/common/js/services/config.js
+++ src/Notification.Web/src/common/js/services/config.js
@@ -2,11 +2,12 @@
  * Created by everton.ferreira on 16/06/2017.
  */
 var Config = {
+    IDENTITY: 'http://localhost:5001/identity',
     API: "http://10.10.10.37:5019/",
     APICoreSSO: "api/CoreSSO/v1",
     APISGP: "api/SGP/v1",
     OIDCCLIENT: {
-        authority: "http://10.10.10.37:5000",
+        authority: "ids-identityserver.devedu.ms",
         client_id: "mstechjs",
         redirect_uri: "http://localhost:5003/callback.html",
         response_type: "id_token token",
