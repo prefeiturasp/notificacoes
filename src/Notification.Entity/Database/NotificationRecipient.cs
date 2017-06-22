fd64436899ffac61356bebbeb1507357c3acfcc7
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-06-22 12:22:49 -0300

    [API] Adicionado método para salvar notificação por Tipo de Sistema
     - Apenas para usuário com Visão Administrador

---
+++ src/Notification.Entity/Database/NotificationRecipient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Entity.Database
+{
+    public class NotificationRecipient
+    {
+        public Guid UserId { get; set; }        
+    }
+}
