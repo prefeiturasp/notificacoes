c43d80d2ef148aab8936c26135a2ce9d27944bcf
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-06-24 10:09:39 -0300

    [API SGP] Criado a consulta de Periodo (CoursePeriod)

--- src/Notification.Repository/SGP/CoursePeriodRepository.cs
+++ src/Notification.Repository/SGP/CoursePeriodRepository.cs
@@ -12,34 +12,25 @@ namespace Notification.Repository.SGP
 {
     public class CoursePeriodRepository : SGPRepository
     {
-        public IEnumerable<CoursePeriod> Get()
+        public IEnumerable<CoursePeriod> Get(string calendarYear, IEnumerable<int> courseId)
         {
             using (var context = new SqlConnection(stringConnection))
             {
-                var query = context.Query<CoursePeriod>(
-                    @"SELECT
-	                    crp.cur_id,
-	                    crp.crr_id,
-	                    crp.crp_id,
-	                    crp.crp_ordem,
-	                    crp.crp_descricao,
-	                    crp.crp_controleTempo,
-	                    crp.crp_ciclo,
-	                    crp.crp_concluiNivelEnsino,
-	                    crp.tcp_id,
-	                    crp.tci_id
-                    FROM
-	                    ACA_Curso cur WITH(NOLOCK)
-	                    INNER JOIN ACA_Curriculo crr WITH(NOLOCK)
-		                    ON crr.cur_id = cur.cur_id
-		                    AND crr.crr_situacao <> 3
-	                    INNER JOIN ACA_CurriculoPeriodo crp WITH(NOLOCK)
-		                    ON crp.cur_id = crr.cur_id
-		                    AND crp.crr_id = crr.crr_id
-		                    AND crp.crp_situacao <> 3
-                    WHERE
-	                    cur.cur_situacao <> 3
-	                    AND cur.cur_id IN @idsCurso");
+                StringBuilder sb = new StringBuilder();
+                sb.Append(@"SELECT cast(crp.cur_id as varchar(8)) + '|' + cast(crp.crr_id as varchar(8)) + '|' + cast(crp.crp_id as varchar(8)) as Id, crp.crp_descricao as Name, crp.crp_descricao
+                    FROM ACA_Curso cur WITH(NOLOCK)
+	                INNER JOIN ACA_Curriculo crr WITH(NOLOCK) ON crr.cur_id = cur.cur_id AND crr.crr_situacao <> 3
+	                INNER JOIN ACA_CurriculoPeriodo crp WITH(NOLOCK) ON crp.cur_id = crr.cur_id AND crp.crr_id = crr.crr_id AND crp.crp_situacao <> 3
+	                INNER JOIN ACA_calendarioCurso AS CAC WITH(NOLOCK) on cac.cur_id = cur.cur_id
+	                INNER JOIN ACA_CalendarioAnual AS CAL WITH(NOLOCK) on cal.cal_id = cac.cal_id
+                    WHERE cur.cur_situacao <> 3
+		                AND CAL.cal_ano = @calendarYear");
+
+                if (courseId != null && courseId.Any())
+                    sb.Append(" AND cur.cur_id IN @courseId");
+                
+                var query = context.Query<CoursePeriod>(sb.ToString(),
+                    new { calendarYear = calendarYear, courseId = courseId });
                 return query;
             }
         }
