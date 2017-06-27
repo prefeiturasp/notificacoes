403f70b853a32ed5febca4ee70bda2c0829acc46
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-06-27 11:43:30 -0300

    [API Notificação] Adicionado a Busca de notificação por Id e oir UserId

--- src/Notification.Repository/NotificationRepository.cs
+++ src/Notification.Repository/NotificationRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Notification.Entity.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +15,42 @@ namespace Notification.Repository
         {
             Collection.InsertOne(entity);
             return entity.Id;
+        }
+
+        public NotificationPlugin GetById(Guid id)
+        {
+            var result = Collection.Find(n => n.Id == id).FirstOrDefault();
+
+            if (result == null)
+                return null;
+
+            return new NotificationPlugin()
+            {
+                Id = result.Id,
+                SenderName = result.SenderName,
+                DateStartNotification = result.DateStartNotification,
+                DateEndNotification = result.DateEndNotification,
+                MessageType = result.MessageType,
+                Title = result.Title,
+                Message = result.Message
+            };
+        }
+
+        public IEnumerable<NotificationPlugin> GetByUserId(Guid userId)
+        {
+            var result = Collection
+                .Find(n => n.Recipient.Any(r => r.UserId == userId))
+                .Project(x => new NotificationPlugin() {
+                    Id = x.Id,
+                    SenderName = x.SenderName,
+                    DateStartNotification = x.DateStartNotification,
+                    DateEndNotification = x.DateEndNotification,
+                    MessageType = x.MessageType,
+                    Title = x.Title,
+                    Message = x.Message
+                });
+            
+            return result.ToList();            
         }
     }
 }
