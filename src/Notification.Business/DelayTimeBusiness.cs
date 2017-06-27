d53597a13ee5cc267e3cca26e7ea462ede78f779
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-06-27 12:34:39 -0300

    [API Notificação] Criado a consulta de Tempos
     - Tempo de Não Pertube
     - Tempo de Adiar

---
+++ src/Notification.Business/DelayTimeBusiness.cs
@@ -0,0 +1,25 @@
+﻿using Notification.Entity.Database;
+using Notification.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Business
+{
+    public class DelayTimeBusiness
+    {
+        public static int Save(DelayTime entity)
+        {
+            var repository = new DelayTimeRepository();
+            return repository.InsertOne(entity);
+        }
+
+        public static IEnumerable<DelayTime> Get()
+        {
+            var repository = new DelayTimeRepository();
+            return repository.Get();
+        }
+    }
+}
