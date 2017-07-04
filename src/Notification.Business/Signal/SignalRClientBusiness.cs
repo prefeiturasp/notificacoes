9b15cb3972f648e04b0ca40850dae48d8d0dfb2e
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-07-04 12:11:29 -0300

    [API Notificação] Adicionado configuração de Credential do SinglaR

--- src/Notification.Business/Signal/SignalRClientBusiness.cs
+++ src/Notification.Business/Signal/SignalRClientBusiness.cs
@@ -11,8 +11,12 @@ namespace Notification.Business.Signal
     public class SignalRClientBusiness
     {
         public const string CONFIG_URLSIGNALRSERVERHUB = "UrlSignalRServerHub";
+        public const string CONFIG_USERCREDENTIALSIGNALRSERVER = "UserCredentialSignalRServer";
+        public const string CONFIG_PASSWORDCREDENTIALSIGNALRSERVER = "PasswordCredentialSignalRServer";
 
         public static string UrlSignalRServer = string.Empty;
+        public static string UserCredentialSignalRServer = string.Empty;
+        public static string PasswordCredentialSignalRServer = string.Empty;
 
         public static void SendNotificationHangFire(IEnumerable<Guid> users, Guid notificationId)
         {
@@ -29,20 +33,44 @@ namespace Notification.Business.Signal
 
         public static void SendNotification(IEnumerable<Guid> users, Guid notificationId)
         {
-            var notif = NotificationBusiness.GetById(notificationId);
-            var notifP = new Notification.Entity.SignalR.Notification()
+            try
             {
-                Id = notif.Id,
-                Title = notif.Title,
-                Message = notif.Message
-            };
+                var notif = NotificationBusiness.GetById(notificationId);
 
-            var hubConnection = new HubConnection(UrlSignalRServer);
-            var hub = hubConnection.CreateHubProxy("notificationHub");
+                if (notif == null)
+                {
+                    LogBusiness.Warn(string.Format("[SignalRClientBusiness] SendNotification - Notificação não encontrada ({0})", notificationId));
+                    return;
+                }
 
-            hubConnection.Start().Wait();
-            hub.Invoke("SendNotification", users, notifP);
-            hubConnection.Stop();
+                var notifP = new Notification.Entity.SignalR.Notification()
+                {
+                    Id = notif.Id,
+                    Title = notif.Title,
+                    Message = notif.Message
+                };
+
+                var hubConnection = new HubConnection(UrlSignalRServer);
+                var hub = hubConnection.CreateHubProxy("notificationHub");
+
+                hubConnection.Headers.Add("Authorization", "Basic " + Base64Encode(UserCredentialSignalRServer, PasswordCredentialSignalRServer));
+
+                hubConnection.Start().Wait();
+                hub.Invoke("SendNotification", users, notifP);
+                hubConnection.Stop();
+            }
+            catch (Exception exc)
+            {
+                LogBusiness.Error(exc);
+                throw;
+            }
+        }
+
+        private static string Base64Encode(string user, string password)
+        {
+            var str = string.Format("{0}:{1}", user, password);
+            var plainTextBytes = System.Text.Encoding.ASCII.GetBytes(str);
+            return System.Convert.ToBase64String(plainTextBytes);
         }
     }
 }
