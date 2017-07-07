baa9eae4d1cbb77b2f9b2377cc32362cf44a7ab7
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-07-07 14:19:52 -0300

    [API] Busca de Sites aceitos na API
     - Adicionado uma busca no IDS_ClientCorsOrigins para adicionar como sites aceitos para requisitar  a API

---
+++ src/Notification.Entity/Database/CoreSSO/IDSClientCorsOrigins.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Entity.Database.CoreSSO
+{
+    public class IDSClientCorsOrigins
+    {
+        public int Id { get; set; }
+
+        public int ClientId { get; set; }
+
+        public string Origin { get; set; }
+    }
+}
