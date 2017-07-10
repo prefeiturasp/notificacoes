3d0f7f498c650153ec0ae8fbd46ce83705e1f930
André Ruz <andreruz@gmail.com>
2017-07-10 16:56:07 -0300

    remover da listagem de grupos o visão individual

--- src/Notification.Repository/CoreSSO/GroupRepository.cs
+++ src/Notification.Repository/CoreSSO/GroupRepository.cs
@@ -52,7 +52,11 @@ namespace Notification.Repository.CoreSSO
                     @"SELECT G.gru_id AS Id, G.gru_nome AS Name, G.sis_id as SystemId, G.vis_id AS VisionId
 	                    FROM SYS_UsuarioGrupo AS UG WITH(NOLOCK)
 	                    INNER JOIN SYS_Grupo AS G WITH(NOLOCK) ON UG.gru_id = G.gru_id
-                        WHERE UG.usg_situacao = 1 AND G.gru_situacao = 1 AND G.sis_id = @systemId AND UG.usu_id = @userId
+                        WHERE UG.usg_situacao = 1 
+                            AND G.gru_situacao = 1 
+                            AND G.sis_id = @systemId 
+                            AND UG.usu_id = @userId
+                            AND G.vis_id < 4
                         ORDER BY G.gru_nome",
                     new { systemId = systemId, userId = userId });
                 return query;
