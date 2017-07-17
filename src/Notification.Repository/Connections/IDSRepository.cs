fb527824f202662fd6af1b8d6d80823bd77d5d49
MSTECH\Thiago.froes <thiago.froes@mstech.com.br>
2017-07-17 18:31:12 -0300

    [API] Criado a conexão IDS

---
+++ src/Notification.Repository/Connections/IDSRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Repository.Connections
+{
+    public abstract class IDSRepository
+    {
+        internal readonly string stringConnection;
+
+        public IDSRepository()
+        {
+            stringConnection = Connection.Get("IDS");
+        }
+    }
+}
