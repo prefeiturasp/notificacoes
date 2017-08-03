fdada99febcf5524bf9db2cffb4b117f0fd77e4f
MSTECH\everton.ferreira <everton.ferreira@mstech.com.br>
2017-08-03 15:59:34 -0300

    correção no config.js salvando xml no storage

--- src/Notification.Web/config.js
+++ src/Notification.Web/config.js
@@ -18,21 +18,28 @@
         post_logout_redirect_uri: "http://notificacao.mstech.com.br/index.html"
     }
 };*/
+
 var Config = null;
 var x2js = new X2JS();
-
 function loadXMLDoc() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      callback(this);
-    }
-  };
-  xmlhttp.open("GET", "config.xml", true);
-  xmlhttp.send();
+	
+	if(!window.sessionStorage.config){	
+	  var xmlhttp = new XMLHttpRequest();
+	  xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		  callback(this);
+		}
+	  };
+	  xmlhttp.open("GET", "config.xml", true);
+	  xmlhttp.send();
+	  
+	}else{
+		Config = $.parseJSON(atob(window.sessionStorage.config));
+	}
 }
 function callback(xml) {  
-  Config = $.parseJSON(JSON.stringify(x2js.xml_str2json(xml.responseText))).CONFIG;  
+	Config = $.parseJSON(JSON.stringify(x2js.xml_str2json(xml.responseText))).CONFIG; 	
+	window.sessionStorage.config = btoa(JSON.stringify(Config));
 }
 
 loadXMLDoc();
