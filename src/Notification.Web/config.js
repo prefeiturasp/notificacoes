7d56a52dec4ccb2d18ed3ffeb9b38dab36df8718
MSTECH\everton.ferreira <everton.ferreira@mstech.com.br>
2017-08-03 15:10:13 -0300

    alterando config json para um config em xml para converter em json de pois

--- src/Notification.Web/config.js
+++ src/Notification.Web/config.js
@@ -1,7 +1,7 @@
 /**
  * Created by everton.ferreira on 16/06/2017.
  */
-var Config = {
+/*var Config = {
     URL_SIGGNALR: 'http://notificacaoService.mstech.com.br/signalr',
     IDENTITY: 'http://coressoids.mstech.com.br/identity',
     API: "http://notificacaoApi.mstech.com.br",
@@ -17,4 +17,22 @@ var Config = {
         scope: "openid profile mstechapi",
         post_logout_redirect_uri: "http://notificacao.mstech.com.br/index.html"
     }
-};
+};*/
+var Config = null;
+var x2js = new X2JS();
+
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this);
+    }
+  };
+  xmlhttp.open("GET", "config.xml", true);
+  xmlhttp.send();
+}
+function callback(xml) {  
+  Config = $.parseJSON(JSON.stringify(x2js.xml_str2json(xml.responseText))).CONFIG;  
+}
+
+loadXMLDoc();
