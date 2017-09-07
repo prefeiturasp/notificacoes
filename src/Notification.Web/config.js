391b207b3cb77271d43a2399801c8857cbe394f1
Juliano Real <juliano.real@mstech.com.br>
2017-09-07 17:05:18 -0300

    Altedado o load do config.

--- src/Notification.Web/config.js
+++ src/Notification.Web/config.js
@@ -1,25 +1,16 @@
-/**
- * Created by everton.ferreira on 16/06/2017.
- */
 var Config = null;
 var x2js = new X2JS();
 function loadXMLDoc() {
-    if (!window.sessionStorage.config) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                callback(this);
-            }
-        };
-        xmlhttp.open("GET", "config.xml", true);
-        xmlhttp.send();
+
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
     } else {
-        Config = $.parseJSON(atob(window.sessionStorage.config));
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-}
-function callback(xml) {
-    Config = $.parseJSON(JSON.stringify(x2js.xml_str2json(xml.responseText))).CONFIG;
-    window.sessionStorage.config = btoa(JSON.stringify(Config));
+    xhttp.open("GET", "config.xml", false);
+    xhttp.send();
+    var xmlDoc = xhttp.responseXML;
+    Config = x2js.xml2json(xmlDoc).config;
 }
 
 loadXMLDoc();
